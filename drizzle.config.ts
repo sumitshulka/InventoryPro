e4dba3fb9946cdc4db4cc8816b86ed05abbc6248
Kunal Verma <kunal.verma@smopl.com>
2025-06-16 13:03:19 +0530

    Commit initial start up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+  throw new Error("DATABASE_URL must be set. Ensure the .env file is present.");
 }
 
 export default defineConfig({
--- server/db.ts
+++ server/db.ts
@@ -2,12 +2,16 @@ import pkg from 'pg';
 const { Pool } = pkg;
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "@shared/schema";
+import dotenv from "dotenv";
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
-}
+}else(
+  console.log("DB Connected:", process.env.DATABASE_URL)
+)
 
 // Create pool with better error handling and connection management
 export const pool = new Pool({ 
