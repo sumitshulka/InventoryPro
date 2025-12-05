c8622446558775ff9ca04e4f1514d9c30cc30790
Ribhu <ribhusingh4@gmail.com>
2025-12-05 15:41:32 +0530

    merge ready code 05-12-2025

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from "./App";
 import "./index.css";
 import { queryClient } from "./lib/queryClient";
@@ -8,6 +7,5 @@ import { queryClient } from "./lib/queryClient";
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <App />
-    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
